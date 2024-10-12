8c154356025f560598b8318abe0cf7aa4a8a37b5
Ty Hopp <hopp.ty.c@gmail.com>
2024-10-12 13:52:33 +0800

    feat(js): handle read binary error cases

--- js/main.mjs
+++ js/main.mjs
@@ -1,23 +1,40 @@
 #!/usr/bin/env node
 
-import { readFile } from 'node:fs/promises';
+import fs from 'node:fs/promises';
+import process from 'node:process';
 import { WASI } from 'node:wasi';
-import { argv, env } from 'node:process';
 
 const cwd = process.cwd();
 
 const wasi = new WASI({
   version: 'preview1',
-  args: argv.slice(1),
-  env,
+  args: process.argv.slice(1),
+  env: process.env,
   preopens: {
     [cwd]: cwd,
   },
 });
 
-const wasm = await WebAssembly.compile(
-  await readFile(new URL('./lenv-wasip1-0.1.7.wasm', import.meta.url)),
-);
+const wasiBinaryName = "lenv-wasip1-0.1.7.wasm";
+const wasiBinaryPath = new URL(wasiBinaryName, import.meta.url)
+
+try {
+  await fs.access(wasiBinaryPath, fs.constants.F_OK)
+} catch (_) {
+  console.error(`lenv: wasi binary ${wasiBinaryPath} not found`);
+  process.exit(1);
+}
+
+let wasiBinaryBuffer;
+
+try {
+  wasiBinaryBuffer = await fs.readFile(wasiBinaryPath)
+} catch (_) {
+  console.error(`lenv: wasi binary ${wasiBinaryPath} not readable`);
+  process.exit(1);
+}
+
+const wasm = await WebAssembly.compile(wasiBinaryBuffer);
 const instance = await WebAssembly.instantiate(wasm, wasi.getImportObject());
 
 wasi.start(instance);
